6cfcb866bda523a2ad3f9565b263cf94c37f6e8c
Gadila Shashank Reddy <Gadila Shashank Reddy>
2018-08-12 13:26:22 +0530

    Readme and script added

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
 # Movie-Downloader
-An openload movies wrapper to view or download movies
+
+---
+Version : 1.0
+
+Author : Gadila Shashank Reddy
+
+---
+
+## About
+
+This simple python script is a wrapper over openload movies and helps to explore
+, search and download movies all in a single package via a command line
+interface.
+
+## Installation
+
+This script is supposed to work on all UNIX based systems (GNU-Linux, Mac...)
+I haven't tested this on Windows.
+
+Python3 is only supported. Not tested on python2. The dependencies of this
+script are
+
+* [requests](https://pypi.org/project/requests/)
+
+* [BeautifulSoup 4](https://pypi.org/project/beautifulsoup4/)
+
+* [youtube-dl](https://pypi.org/project/youtube_dl/)
+  * This requires PhantomJS. See [here](http://phantomjs.org/download.html)
+
+Inbuilt libraries used. Make sure these are importable.
+
+* argparse
+* os
+* time
+* unquote and urlparse from urllib.parse
+* webbrowser
+
+## Usage
+
+```md
+$ python3 movie.py -h
+usage: movie.py [-h] [-d] [[-g [G] | -y [Y] | -s [S [S ...]]] [-f | -t | -p]
+
+Openload Movies wrapper
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -d              Optional flag to download if possible
+  -g [G]          Specify a genre
+  -y [Y]          Specify a release year
+  -s [S [S ...]]  Search a movie
+  -f              Featured Movies
+  -t              Top Rated Movies
+  -p              Popular/Most viewed Movies
+```
+
+Using the script is pretty simple. Some examples are given.
+
+* Some usage examples. Note the multi worded search.
+
+```md
+$ python3 movie.py -f
+$ python3 movie.py -f -d
+$ python3 movie.py -g adventure
+$ python3 movie.py -y 2009
+$ python3 movie.py -s avengers infinity war
+```
+
+* Default interface to select a movie to watch/download
+
+Typical results when using the search function.
+
+```md
+28**************************************************
+Beyond The Sky (2018)
+IMDb N/A
+**************************************************
+29**************************************************
+The Perfect Bride: Wedding Bells (2018)
+IMDb 6.6
+**************************************************
+30**************************************************
+Eyes In The Hills (2018)
+IMDb N/A
+**************************************************
+Enter number to watch the movie or 0 for next page
+```
+
+Typical results otherwise.
+
+```md
+28**************************************************
+Ready Player One (2018)
+CAM | 7.4 stars
+**************************************************
+29**************************************************
+Sherlock Gnomes (2018)
+CAM | 8.7 stars
+**************************************************
+30**************************************************
+A Wrinkle in Time (2018)
+TS | 5.3 stars
+**************************************************
+Enter a number to load that movie or 0 to load next page
+```
+
+The only difference in both these type of results is the presence of picture
+quality. In both cases, the rating is out of 10 stars.
+
+At the prompt if you enter 0 then the next page is loaded (if present) and if
+you input a valid movie number, then the script either open the link in a new
+browser window or initiates a download depending on whether the "-d" option is
+specified or not.
+
+All downloads happen in the directory where the script resides.
+
+5 wrong inputs terminates the script.
+
+## Regarding development env and bugs
+
+This script has been developed and tested on python 3.7.0 running on an Arch
+Linux x86_64 machine and is expected to run on python 3.5.x and above.
+
+Incase of any issue with script, do raise an issue on the repository.
+
+Thanks for using the script :)
---
+++ movie.py
@@ -0,0 +1,233 @@
+"""
+Open load movies wrapper.
+User can search, Select from
+from a range of curated lists
+like genre, year of release, featured,
+top or most popular movies.
+The movie is opened in the default
+browser without the hassle of ads
+or aanoying redirects.
+The script cannot detect if the movie
+at the end of the link is deleted or not
+
+python3 movie.py -h
+for more info on and arguments used.
+"""
+import argparse
+from bs4 import BeautifulSoup as bs
+import os
+import requests
+import time
+from urllib.parse import unquote, urlparse
+import webbrowser as wb
+import youtube_dl
+
+# Restrict choices of genre and years(1992 - current year)
+genre_choices = ['action', 'adventure', 'animation',
+                 'biography', 'comedy', 'crime', 'documentary', 'drama',
+                 'family', 'fantasy', 'featured', 'foreign', 'history',
+                 'horror', 'music', 'mystery', 'romance', 'science-fiction',
+                 'short', 'tagalog-dubbed', 'thriller', 'tv-movie', 'upcoming',
+                 'war', 'western']
+
+year_choices = []
+for i in range(1992, int(time.strftime("%Y"))+1):
+    year_choices.append(str(i))
+
+# Define Comamnd Line arguments
+parser = argparse.ArgumentParser(description="Openload Movies wrapper")
+parser.add_argument("-d", action="store_true", help="Optional flag to download \
+                     if possible")
+master = parser.add_mutually_exclusive_group()
+user = master.add_mutually_exclusive_group()
+user.add_argument("-g", nargs="?", help="Specify a genre")
+user.add_argument("-y", help="Specify a release year", nargs="?")
+user.add_argument("-s", help='Search a movie', nargs="*", type=str)
+
+curated = master.add_mutually_exclusive_group()
+curated.add_argument("-f", help="Featured Movies", action="store_true")
+curated.add_argument("-t", help="Top Rated Movies", action="store_true")
+curated.add_argument("-p", help="Popular/Most viewed Movies",
+                     action="store_true")
+
+args = parser.parse_args()
+
+num_true = 0
+for i in vars(args).values():
+    if i:
+        num_true +=1
+
+if num_true <= 1 and args.d:
+    print("Use atleast one other argument with -d option")
+    quit()
+
+# Print help if no argunment is mentioned
+if not any(vars(args).values()):
+    print("Atleast one of the arguments must be mentioned.\n")
+    print(parser.print_help())
+    quit()
+
+
+# Get openload movie link.
+def get_openloadmovie_link():
+    url = "https://duckduckgo.com/html/?q=openload+movie"
+    response = requests.get(url)
+    soup = bs(response.content, "lxml")
+    for i in soup.find_all("a", attrs={"class": "result__a"}):
+        if i.text == "Openload Movies - Watch Free HD Movies Online":
+            return unquote(i['href']).replace("/l/?kh=-1&uddg=", "")
+
+
+# Gets movie list from given URL
+def get_curated_list(url):
+    count = 1
+    url_list = []
+    response = requests.get(url)
+    soup = bs(response.content, "lxml")
+    for i in soup.find_all("div", attrs={"class": "poster"}):
+        try:
+            title = unquote(i.img['alt'])[:-16]
+        except(AttributeError):
+            title = "Unknown title"
+        try:
+            quality = i.find("span", attrs={"class": "quality"}).text.strip()
+        except(AttributeError):
+            quality = "Unknown quality"
+        try:
+            rating = i.find("div", attrs={"class": "rating"}).text.strip()
+        except(AttributeError):
+            rating = "Unknown rating"
+        print(str(count) + "*"*50)
+        print(title)
+        print(quality + " | " + rating + " stars")
+        print("*"*50)
+        url_list.append(i.a['href'])
+        count += 1
+    return url_list
+
+
+# Function to search and fire movie
+def search_movie(query, page):
+    total_pages = 0
+    url_list = []
+    count = 1
+    url = "https://openloadmovie.org/page/"
+    term = url + str(page) + "/?s=" + query
+    response = requests.get(term)
+    soup = bs(response.content, "lxml")
+    for i in soup.find_all("div", attrs={"class": "details"}):
+        url_list.append(i.a['href'])
+        print(str(count) + "*"*50 + "\n" + i.a.text)
+        print(i.find("span", attrs={"class": "rating"}).text + "\n" + "*"*50)
+        count += 1
+    while True:
+        temp = input("Enter number to watch the movie or 0 for next page ")
+        if int(temp) < len(url_list) + 1 and int(temp) != 0:
+            fire_movie(url_list[int(temp) - 1])
+            quit()
+        elif int(temp) == 0:
+            for i in soup.find_all("span", attrs={"class": ""}):
+                total_pages = i.text.split("of")[1].strip()
+            if not total_pages:
+                print("This is the only page. Try again")
+                quit()
+            if int(page) < int(total_pages):
+                search_movie(query, page+1)
+        else:
+            print("Wrong choice")
+            quit()
+
+# Open browser to movie link.
+def fire_movie(url):
+    print(args)
+    os.system("reset")
+    response = requests.get(url)
+    soup = bs(response.content, "lxml")
+    direct_link = soup.find("div", attrs={"id": "option-1"}).iframe['src']
+    print(direct_link)
+    ydl_link = []
+    ydl_link.append(direct_link)
+    print(ydl_link)
+    if args.d:
+        ydl_opts = {}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(ydl_link)
+    else:
+        wb.open(direct_link)
+    quit()
+
+
+url = get_openloadmovie_link()
+print(url)
+if not url:
+    print("URL not found...")
+    print("Try after some time.")
+    quit()
+
+
+# Call relevant function based on arguments
+# and exit on wrong input
+if args.f:
+    if url[-1] == "/":
+        url = url + "genre/featured/page/"
+    else:
+        url = url + "/genre/featured/page/"
+    url_list = get_curated_list(url + "1")
+elif args.t:
+    if url[-1] == "/":
+        url = url + "top-rated/page/"
+    else:
+        url = url + "/top-rated/page/"
+    url_list = get_curated_list(url + "1")
+elif args.p:
+    if url[-1] == "/":
+        url = url + "most-viewed/page/"
+    else:
+        url = url + "/most-viewed/page/"
+    url_list = get_curated_list(url + "1")
+
+elif args.s:
+    search_movie(" ".join(args.s), 1)
+elif args.g:
+    if args.g not in genre_choices:
+        print("Select one of the following genres.")
+        print(genre_choices)
+        quit()
+    if url[-1] == "/":
+        url = url + "genre/" + args.g + "/page/"
+    else:
+        url = url + "/genre/" + args.g + "/page/"
+    url_list = get_curated_list(url + "1")
+
+elif args.y:
+    if args.y not in year_choices:
+        print("Select year between " + str(year_choices[0]) + "-" +
+              str(year_choices[-1]) + " (both included)")
+        quit()
+    if url[-1] == "/":
+        url = url + "release/" + args.y + "/page/"
+    else:
+        url = url + "/release/" + args.y + "/page/"
+    url_list = get_curated_list(url + "1")
+
+page_number = 2
+break_count = 1
+while break_count < 5 and not args.s:
+    temp = input("Enter a number to load that movie or 0 to load next page ")
+    try:
+        if temp == 0:
+            os.system('reset')
+            url_list = get_curated_list(url + str(page_number))
+            page_number += 1
+        elif int(temp) < len(url_list) + 1:
+            fire_movie(url_list[int(temp) - 1])
+            break_count = 10
+        else:
+            break_count += 1
+    except Exception as e:
+        print("\nWrong choice")
+        print("Enter 0 for next page or the number of the movie to watch\n")
+        break_count += 1
+if break_count == 5:
+    print("You've bored me with wrong choices")
+    print("I give up. Try again later")
